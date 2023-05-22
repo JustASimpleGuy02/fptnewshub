11e837588c0622b44f6c6df05685ee58122049fa
Pham Hoang Nam <hoangnampham2412@gmail.com>
2023-05-22 09:31:30 +0700

    adjust crawl text function

--- Crawler/function.py
+++ Crawler/function.py
@@ -11,9 +11,8 @@ def crawl_list_news(link):
     import requests
     
     result = []
-    
-    raw_data = requests.get(link)
-    soup = BeautifulSoup(raw_data.text, 'html.parser')
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
     article_list = soup.find_all("article")
     for article in article_list:
         try:
@@ -21,33 +20,23 @@ def crawl_list_news(link):
             result.append(href)
         except:
             continue
-        
     return result
 
 # return text from a news page
-def crawl_news_text(link_news):
-    from bs4 import BeautifulSoup
+def crawl_news_text(link_news, language = "Vietnamese"):
     import requests
+    import justext
     
     result = []
-    
-    raw_data = requests.get(link_news)
-    
-    soup = BeautifulSoup(raw_data.text, 'html.parser')
-    
-    article = soup.find("article")
-    
-    list_paragraph = article.find_all()
-    
-    t = 0
-    
-    for paragraph in list_paragraph:
-        if len(paragraph.find_all()) == 0:
+    response = requests.get(link_news)    
+    paragraphs = justext.justext(response.content, justext.get_stoplist(language))
+    for paragraph in paragraphs:
+        if not paragraph.is_boilerplate:
             result.append(paragraph.text)
-    
-    return ' '.join(result)
+    return '\n'.join(result)
 
 # print(crawl_list_news('https://vnexpress.net/chu-de/dai-hoc-fpt-2161'))
 # print(crawl_news_text('https://vnexpress.net/tiet-kiem-hang-tram-trieu-hoc-phi-nho-hoc-bong-mba-4606366.html'))
 # print(crawl_news_text('https://hanoi.fpt.edu.vn/cuu-sinh-vien-dai-hoc-fpt-gianh-hoc-bong-tien-si-tai-phap.html'))
-# print(crawl_news_text('https://hanoi.fpt.edu.vn/doc-dao-do-an-thiet-ke-bo-nhan-dien-thuong-hieu-cho-lien-doan-xiec-viet-nam-cua-sinh-vien-dai-hoc-fpt.html'))
+# print(crawl_news_text('https://guu.vn/diem-tin/tp-hcm-bo-lai-xe-may-nam-sinh-vien-dai-hoc-fpt-nghi-nhay-tu-cau-vuot-xuong-dat-tu-tu-5dc935d7a35d7d08076d091f.html'))
+# print(crawl_news_text("https://congan.com.vn/vu-an/nam-sinh-dai-hoc-fpt-chan-duong-sat-hai-ban-gai-tu-tu-bat-thanh_134260.html"))
