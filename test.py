0e849e553d27bc40056b29706112236fc5a49071
Pham Hoang Nam <hoangnampham2412@gmail.com>
2023-05-28 11:40:33 +0700

    crawl more link

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
-from serpapi import GoogleSearch
-import json
+from selenium import webdriver
+import time
 
-params = {
-    "api_key": "...",           # https://serpapi.com/manage-api-key
-    "engine": "google",         # serpapi parsing engine
-    "q": "đại học fpt",     # search query
-    "gl": "vn",                 # country from where search comes from
-    "tbm": "nws"                # news results
-    # other parameters such as language `hl` and number of news results `num`, etc.
-}
+# start web browser
+browser=webdriver.Chrome()
 
-search = GoogleSearch(params)   # where data extraction happens on the backend
-results = search.get_dict()     # JSON - > Python dictionary
-print(results)
-# for result in results["news_results"]:
-    # print(json.dumps(results, indent=2))
+# get source code
+browser.get("https://www.google.com/search?q=đại+học+fpt+site:cand.com.vn&num=100&start=0")
+time.sleep(100)
+html = browser.page_source
+print(html)
+
+# close web browser
+browser.close()
